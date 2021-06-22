b464544ac885fe59fbff300785c4b047afe6b6ae
proctorstore <proctorestore999@gmail.com>
2021-06-22 18:50:06 +0530

    Add pytest

--- weather/tests.py
+++ weather/tests.py
@@ -2,21 +2,9 @@ from django.test import TestCase
 from weather.models import City
 
 # Create your tests here.
-print("running tests")
+print("Running tests")
+
 class CityTestCase(TestCase):
-    def setUp(self):
-        City.objects.create(name="Kolkata")
-        City.objects.create(name="Kadapa")
-    
-    def test_city_names(self):
-        city_1 = City.objects.get(name="Kolkata")
-        city_2 = City.objects.get(name="Kadapa")
-
-        self.assertEqual(city_1.name, 'Kolkata')
-        self.assertEqual(city_2.name, 'Kadapa')
-
-
-class CityTestCase2(TestCase):
     def setUp(self):
         City.objects.create(name="Kolkata")
         City.objects.create(name="Kadapa")
